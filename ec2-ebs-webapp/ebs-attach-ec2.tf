8e875027290cb443e6c2ab0586cdb9d62039cf65
Umesh Tyagi <54307317+umeshtyagi829@users.noreply.github.com>
2021-05-15 11:38:57 +0530

    Delete ec2-ebs-webapp directory

--- ec2-ebs-webapp/ebs-attach-ec2.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_volume_attachment" "ebs_att" {
-device_name = "/dev/sdh"
-volume_id = aws_ebs_volume.st1.id
-instance_id = aws_instance.os1.id
-}
-
-
--- ec2-ebs-webapp/ebs.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_ebs_volume" "st1" {
-availability_zone = aws_instance.os1.availability_zone
-size = 1
-tags = {
-Name = "HD1"
-}
-}
-
